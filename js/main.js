a5929d105cfb9dbc52511632172a0e9a8795a894
Marco <marcosinisa.mihajlovic@gmail.com>
2021-02-18 13:43:22 +0100

    Esercizio Completato

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Inserisci il tuo colore preferito');
+
+alert('La tua password generata è ' + nome + cognome + colore + '19')
