b2cd3a4a93f8bbf69afd366b3c205975cb16fb32
Marco <marcosinisa.mihajlovic@gmail.com>
2021-02-18 14:44:04 +0100

    Comresso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colore = prompt('Inserisci il tuo colore preferito');
+var password = nome + cognome + colore + '19';
 
-alert('La tua password generata è ' + nome + cognome + colore + '19')
+alert('La tua password generata è ' + password);
+
+document.getElementById('my_id').innerHTML = password;
